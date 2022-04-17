b88d1d75cdc3681a7f6bcc7e9020e75afe65856d
abhix18 <abi.xa12@gmail.com>
2022-04-17 15:58:35 +0530

    homepage components completed

--- src/Product components/Product.js
+++ src/Product components/Product.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import './Product.css'
-function Product() {
-  return (
-    <div className='product'>
-        <div className='product_info'>
-            <p>product description</p>
-            
-        </div>
+function Product({title,image,price,rating}) {
+    return (
+        <div className='product'>
 
-    </div>
-  )
+            <div className='product_info'>
+                <p>{title}</p>
+                <p className='product_price'>
+                    <small>$</small>
+                    <strong>{price}</strong>
+                </p>
+                <div className='product_rating'>
+                    
+                    {Array(rating).fill().map((_,i)=>( <p>⭐️</p> ))}
+                </div>
+            </div>
+
+            <img src={image} />
+            <button>Add to basket</button>
+        </div>
+    )
 }
 
 export default Product
