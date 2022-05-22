c79663d8b393000213ff589775ef021391c2f38e
abhix18 <abi.xa12@gmail.com>
2022-05-22 14:27:59 +0530

    Updates

---
+++ precache-manifest.f08d3d42a9119459a3ff0842facb8004.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b04ff892b47f53700069",
+    "url": "/Amazon-Clone-FullStack/static/css/main.6c925f4a.chunk.css"
+  },
+  {
+    "revision": "b04ff892b47f53700069",
+    "url": "/Amazon-Clone-FullStack/static/js/main.b04ff892.chunk.js"
+  },
+  {
+    "revision": "419f9d958947d7c6749c",
+    "url": "/Amazon-Clone-FullStack/static/js/1.419f9d95.chunk.js"
+  },
+  {
+    "revision": "4174f0087842e555ba05",
+    "url": "/Amazon-Clone-FullStack/static/js/runtime~main.4174f008.js"
+  },
+  {
+    "revision": "d78db2aa3839f27da633769b72bce328",
+    "url": "/Amazon-Clone-FullStack/index.html"
+  }
+];
